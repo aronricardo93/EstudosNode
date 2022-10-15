d5c3bd3a74ffddeba459f57c0afb387c4e305d47
aronricardo93 <aronricardo@hotmail.com>
2022-10-15 20:25:49 -0300

    testes de rotas e da função sendFile

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-    res.send("Seja bem-vindo!")
+    res.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/sobre", function(req,res){
+    res.sendFile(__dirname + "/html/sobre.html")
 })
 
 app.get("/cadastro/:cargo/:nome", function(req,res){

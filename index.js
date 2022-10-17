84f9c5f68bea955ed99c278e28e01e87614b20f4
aronricardo93 <aronricardo@hotmail.com>
2022-10-17 15:12:55 -0300

    refatoração

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require("express")
 const app = express()
+const handlebars = require('express-handlebars')
+const Post = require("./models/Post")
+const post = require("./models/Post")
 
-app.get("/", function(req,res){
+//Configurações
+    //template engine
+    app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
+    app.set('view engine', 'handlebars')
+
+//Capturar os dados do formulário e envia-los
+    app.use(express.urlencoded({extended:false}))
+    app.use(express.json())
+
+
+//rotas
+app.get('/', function(req,res){
+    post.findAll().then(function(posts){
+        res.render('layouts/home', {posts: posts.map(posts => posts.toJSON())})
+    })
+})
+
+app.get('/cadastro', function(req,res){
+    res.render('layouts/formulario')
+})
+
+app.post('/add', function(req,res){
+    post.create({
+        titulo : req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function(){
+        //res.send('Post criado com sucesso!')
+        res.redirect('/')
+    }).catch(function(erro){
+        res.send("Houve um erro " + erro + "!")
+    })
+})
+
+/*app.get("/", function(req,res){
     res.sendFile(__dirname + "/html/index.html")
 })
 
@@ -12,8 +48,6 @@ app.get("/sobre", function(req,res){
 app.get("/cadastro/:cargo/:nome", function(req,res){
     res.send("Olá, " + req.params.nome + "! "
      + "Cargo: " + req.params.cargo)
-})
+})*/
 
-app.listen(8080, function(){
-   console.log("Server running...")
-})
+app.listen(8080)
